0e3666c6fc8f77855483e6bde92a4aec01394d48
ChaseTeague <teaguechase@yahoo.com>
2023-08-12 03:24:52 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(someValue = "roller-skate"){
+    return `This Saturday, I want to ${someValue}!`;
+}
+let mondayWork = function(someValue = 'go to the office'){
+    return `This Monday, I will ${someValue}.`;
+}
+let wrapAdjective = function(value="*"){
+    return function(value2 = "special"){
+        return `You are ${value}${value2}${value}!`
+    }
+}
